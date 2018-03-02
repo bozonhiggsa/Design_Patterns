8dd3f932896823a67fce2d4a94c6c2b6aea14134
Ihor Savchenko <ihor.v.savchenko@gmail.com>
2018-03-02 23:28:02 +0200

    add FileAppender to logback

--- src/main/java/com/designPatterns/patterns/abstractFactory/MainForAbstractFactory.java
+++ src/main/java/com/designPatterns/patterns/abstractFactory/MainForAbstractFactory.java
@@ -1,5 +1,9 @@
 package com.designPatterns.patterns.abstractFactory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
 /**
  * Entry point
  * @author Ihor Savchenko
@@ -7,16 +11,28 @@ package com.designPatterns.patterns.abstractFactory;
  */
 public class MainForAbstractFactory {
 
+    private static final Logger logger = LoggerFactory.getLogger(MainForAbstractFactory.class);
+
     public static void main(String[] args) {
+
+        MDC.put("LOGGERID","entity");
 
         Client client = new Client();
 
+        logger.info("Client is created");
+
+        MDC.put("LOGGERID","service");
+
         client.setFactory1();
+
+        logger.info("Factory 1 is ready");
 
         client.getFactory().createA().methodA();
         client.getFactory().createB().methodB();
 
         client.setFactory2();
+
+        logger.info("Factory 1 is ready");
 
         client.getFactory().createA().methodA();
         client.getFactory().createB().methodB();
