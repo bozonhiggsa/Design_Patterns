977f105f65829edfeb43efcebab098bcdfb4ddf7
Ihor Savchenko <ihor.v.savchenko@gmail.com>
2018-03-05 23:16:36 +0200

    add Singleton based on static nested class

---
+++ src/main/java/com/designPatterns/patterns/singleton/singleton1/Singleton.java
@@ -0,0 +1,21 @@
+package com.designPatterns.patterns.singleton.singleton1;
+
+/**
+ * Singleton based on a private static nested class
+ * @author Ihor Savchenko
+ * @version 1.0
+ */
+public class Singleton {
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    private static class SingletonHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+}
+
