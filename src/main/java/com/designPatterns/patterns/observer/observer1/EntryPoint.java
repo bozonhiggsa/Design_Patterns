12b5aa175a7c3974d1eb11e8ca0d1c689f0a1cd6
Ihor Savchenko <ihor.v.savchenko@gmail.com>
2018-03-07 21:34:48 +0200

    add anonymous observer to pattern Observer

--- src/main/java/com/designPatterns/patterns/observer/observer1/EntryPoint.java
+++ src/main/java/com/designPatterns/patterns/observer/observer1/EntryPoint.java
@@ -1,11 +1,19 @@
 package com.designPatterns.patterns.observer.observer1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Entry point for analyzing the design pattern Observer
+ * One of the observers is based on an anonymous class
  * @author Ihor Savchenko
  * @version 1.0
  */
 public class EntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(EntryPoint.class);
 
     public static void main(String[] args) {
 
@@ -20,6 +28,13 @@ public class EntryPoint {
         eventSource.addObserver(observer1);
         eventSource.addObserver(observer2);
         eventSource.addObserver(observer3);
+
+        eventSource.addObserver(new ObserverReaction() {
+            @Override
+            public void react(AtomicInteger currentState) {
+                logger.info("Observer based on an anonymous class reacted on the event " + currentState);
+            }
+        });
 
         eventSource.eventHappened();
         eventSource.eventHappened();
