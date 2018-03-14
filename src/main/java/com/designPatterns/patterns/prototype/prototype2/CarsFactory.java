0ae1592d57429d278de6019e8a2cec40b2babf20
Ihor Savchenko <ihor.v.savchenko@gmail.com>
2018-03-14 22:40:10 +0200

    supplement of a prototype

---
+++ src/main/java/com/designPatterns/patterns/prototype/prototype2/CarsFactory.java
@@ -0,0 +1,36 @@
+package com.designPatterns.patterns.prototype.prototype2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Factory that registers initial objects for prototyping and
+ * creates prototypes
+ * @author Ihor Savchenko
+ * @version 1.0
+ */
+public class CarsFactory {
+
+    private static final List<Car> prototypes = new ArrayList();
+    private final static UnknownModel unknownModel = new UnknownModel();
+
+    public static void initializePrototypes(){
+        registerPrototype(new Opel());
+        registerPrototype(new Mazda());
+        registerPrototype(new Audi());
+    }
+
+    public static Car getPrototype(String kind) {
+
+        for (Car car: prototypes) {
+            if(car.getModel().equals(kind)){
+                return car.clone();
+            }
+        }
+        return unknownModel.clone();
+    }
+
+    private static void registerPrototype(Car car){
+        prototypes.add(car);
+    }
+}
