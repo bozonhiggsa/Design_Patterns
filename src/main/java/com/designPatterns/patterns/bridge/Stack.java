1e4063263e0944446cc6088cb93b86fc8ad183db
Ihor Savchenko <ihor.v.savchenko@gmail.com>
2018-05-16 00:50:09 +0300

    amending code Bridge

--- src/main/java/com/designPatterns/patterns/bridge/Stack.java
+++ src/main/java/com/designPatterns/patterns/bridge/Stack.java
@@ -3,6 +3,11 @@ package com.designPatterns.patterns.bridge;
 /**
  * Interface/wrapper class that "has a"
  * implementation object and delegates all requests to it.
+ * Bridge is a synonym for the "handle/body" idiom.
+ * This is a design mechanism that encapsulates an implementation class
+ * inside of an interface class.
+ * Bridge is designed up-front to let the abstraction and
+ * the implementation vary independently.
  * @author Ihor Savchenko
  * @version 1.0
  */
