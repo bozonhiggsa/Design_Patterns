c03382f8d512528fd7e8add2ca9ca1fe4416e1b4
Ihor Savchenko <ihor.v.savchenko@gmail.com>
2018-05-16 23:23:11 +0300

    amending code Composite

--- src/main/java/com/designPatterns/patterns/composite/Component.java
+++ src/main/java/com/designPatterns/patterns/composite/Component.java
@@ -1,8 +1,10 @@
 package com.designPatterns.patterns.composite;
 
 /**
- * "Lowest common denominator" that makes
- * classes interchangeable.
+ * "Lowest common denominator" interface that makes
+ * containers and containees classes interchangeable.
+ * It should specify the behavior that needs to be exercised uniformly
+ * across all containee and container objects.
  * @author Ihor Savchenko
  * @version 1.0
  */
